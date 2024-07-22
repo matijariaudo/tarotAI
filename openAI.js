5e0cd51b5680293ab9b2f290cc74053fd5745451
Mati Ariaudo <98933246+matijariaudo@users.noreply.github.com>
2024-07-23 09:23:28 +1000

    front end changes

--- openAI.js
+++ openAI.js
@@ -10,7 +10,7 @@ client = new OpenAI({
   apiKey:apiKeyOpenAI
 })
 consulta =await client.chat.completions.create({
-    model:"gpt-3.5-turbo",
+    model:"gpt-4o-mini",
     response_format:{ "type": "json_object" },
     messages:[
     {
@@ -19,11 +19,13 @@ consulta =await client.chat.completions.create({
        Acts as a tarot reader. We provide tarot services and you will be our tarot reader.
        Answer Format: JSON
        1)language: Language detected in user question
-       2)Objects array called cards. "cards" fields: 
+       2)Objects array(size 3) called cards. 
+        "cards" fields: 
         a)nameCard:(String name).
-        b)Meaning: String(>300 <400 words)giving answer to user question. Write as if you were a positive tarot reader, try to do predictions.
-       3)resume: String (>1000 <1200 words) IMPORTANT:Don't repeat what you put on each individual card. Make a closure. Answer the question base on 3 cards.
-       Notes: 2(a & b),3 must writing in language detected in 1.
+        b)meaning: String(between 80 and 100 words)giving answer to user question. Write as if you were a positive tarot reader, try to do predictions.
+       3)resume: String (between 150 and 180 words) IMPORTANT:Don't repeat what you put on each individual card. Make a closure. Answer the question base on 3 cards.
+       Notes: 2(a & b),3 must writing in language detected in 1. There must be 3 letters and you must respect the indicated json format.
+       When possible try to empathize, phrases like "I know that sometimes you are... but this time", horoscoponegro.com style.
        ` 
     },{
         "role": "user", 
@@ -31,6 +33,7 @@ consulta =await client.chat.completions.create({
     }
     ]
 })
+    console.log(consulta)
     const jsonRta=JSON.parse(consulta?.choices[0]?.message.content);
     return jsonRta;
 }
